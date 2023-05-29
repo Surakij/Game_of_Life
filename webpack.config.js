c8d22002e64fd94888b80fba824705ddce5a7328
Sergey Kazhaev <surakij6@gmail.com>
2023-05-29 22:47:13 +0400

    set up babel for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,22 @@ module.exports = {
     path: resolve(__dirname, "dist"),
     filename: "bundle.js",
     clean: true,
+    environment: {
+      arrowFunction: false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
   },
 };
