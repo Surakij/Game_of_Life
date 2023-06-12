d2cf1c3eda519dba194854a2c99418400c014b8f
Sergey Kazhaev <surakij6@gmail.com>
2023-06-12 17:43:49 +0400

    delete Sanity check

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { sum } from "./sum";
 
 sum(2, 3);
 
-const h1 = document.body.querySelector("h1");
+const h1 = document.body.querySelector("h1") as HTMLElement;
 h1.textContent = "Hello, pal";
